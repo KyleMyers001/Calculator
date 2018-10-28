2da980da5a0bdad2e8874d948cd0e571404902be
KyleMyers001 <41594360+KyleMyers001@users.noreply.github.com>
2018-10-28 13:46:26 -0400

    Update src/store.js

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVT
 const initialState = {};
 const middleWare = [];
 const enhancer = () => {
-  if (reduxDevTools !== null) {
+  if (typeof(reduxDevTools) !== 'undefined') {
     return compose(applyMiddleware(...middleWare), reduxDevTools);
   }
   return applyMiddleware(...middleWare);
