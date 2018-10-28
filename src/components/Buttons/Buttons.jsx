1e8d06534c30d75909816a89e6bbd221ddc4d06c
KyleMyers001 <41594360+KyleMyers001@users.noreply.github.com>
2018-10-28 13:05:24 -0400

    Includes font awesome and improves the UI.

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -7,24 +7,41 @@ const Buttons = (props) => (
   <div className="calculator__btn-wrapper">
     <button className="calculator__btn-operator" onClick={props.resetCurrentNumber}>CE</button>
     <button className="calculator__btn-operator" onClick={props.resetCalculator}>C</button>
-    <button className="calculator__btn-operator calculator__btn-operator--remove-digit" onClick={props.removeDigit}>&#x279C;</button>
-    <button className="calculator__btn-operator" onClick={props.changeOperator.bind(this, operators.divide)}>&divide;</button>
+    <button className="calculator__btn-operator" onClick={props.removeDigit}>
+      <i className="fal fa-backspace"></i>
+    </button>
+    <button className="calculator__btn-operator" onClick={props.changeOperator.bind(this, operators.divide)}>
+      <i className="fal fa-divide"></i>
+    </button>
     <button className="calculator__btn-number" onClick={props.changeNumber.bind(this, '7')}>7</button>
     <button className="calculator__btn-number" onClick={props.changeNumber.bind(this, '8')}>8</button>
     <button className="calculator__btn-number" onClick={props.changeNumber.bind(this, '9')}>9</button>
-    <button className="calculator__btn-operator" onClick={props.changeOperator.bind(this, operators.multiply)}>&times;</button>
+    <button className="calculator__btn-operator" onClick={props.changeOperator.bind(this, operators.multiply)}>
+      <i className="fal fa-times"></i>
+    </button>
     <button className="calculator__btn-number" onClick={props.changeNumber.bind(this, '4')}>4</button>
     <button className="calculator__btn-number" onClick={props.changeNumber.bind(this, '5')}>5</button>
     <button className="calculator__btn-number" onClick={props.changeNumber.bind(this, '6')}>6</button>
-    <button className="calculator__btn-operator" onClick={props.changeOperator.bind(this, operators.subtract)}>&minus;</button>
+    <button className="calculator__btn-operator" onClick={props.changeOperator.bind(this, operators.subtract)}>
+      <i className="fal fa-minus"></i>
+    </button>
     <button className="calculator__btn-number" onClick={props.changeNumber.bind(this, '1')}>1</button>
     <button className="calculator__btn-number" onClick={props.changeNumber.bind(this, '2')}>2</button>
     <button className="calculator__btn-number" onClick={props.changeNumber.bind(this, '3')}>3</button>
-    <button className="calculator__btn-operator" onClick={props.changeOperator.bind(this, operators.add)}>+</button>
-    <button className="calculator__btn-operator" onClick={props.changeNumberState}>&plusmn;</button>
+    <button className="calculator__btn-operator" onClick={props.changeOperator.bind(this, operators.add)}>
+      <i className="fal fa-plus"></i>
+    </button>
+    <button className="calculator__btn-operator calculator__btn-operator__plus-minus" onClick={props.changeNumberState}>
+      <i className="fal fa-plus"></i>
+      <i className="fal fa-minus"></i>
+    </button>
     <button className="calculator__btn-number" onClick={props.changeNumber.bind(this, '0')}>0</button>
-    <button className="calculator__btn-operator" onClick={props.addDecimal}>&sdot;</button>
-    <button className="calculator__btn-operator" onClick={props.showTotal}>=</button>
+    <button className="calculator__btn-operator" onClick={props.addDecimal}>
+      <strong>&sdot;</strong>
+    </button>
+    <button className="calculator__btn-operator" onClick={props.showTotal}>
+      <i className="fal fa-equals"></i>
+    </button>
   </div>
 );
 
